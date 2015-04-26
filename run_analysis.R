3fbc7b77940db60e356f3465d2696f0678c8b18f
Bhavish <hakbhavish@gmail.com>
2015-04-26 18:27:33 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,11 @@
 #Part 1 : Assigning variables to various files as described in codeBook
 # used . to represent a relative patname from working directory
-trainset <- read.table("./data/train/X_train.txt")
-trainlabel <- read.table("./data/train/y_train.txt")
-trainsubject <- read.table("./data/train/subject_train.txt")
-testset <- read.table("./data/test/X_test.txt")
-testlabel <- read.table("./data/test/y_test.txt")
-testsubject <- read.table("./data/test/subject_test.txt")
+trainset <- read.table("./Dataset/train/X_train.txt")
+trainlabel <- read.table("./Dataset/train/y_train.txt")
+trainsubject <- read.table("./Dataset/train/subject_train.txt")
+testset <- read.table("./Dataset/test/X_test.txt")
+testlabel <- read.table("./Dataset/test/y_test.txt")
+testsubject <- read.table("./Dataset/test/subject_test.txt")
 ## You can perform all the necessary tasks to view these variables as required. In summary or header or table form.
 #Merge the X sets of test and train tables
 mergeset <- rbind(trainset, testset)
@@ -15,4 +15,8 @@ mergelabel <- rbind(trainlabel, testlabel)
 mergesubject <- rbind(trainsubject, testsubject)
 
 #Part 2 :Extract the measurements on the mean and standard deviation for each measurement. 
-
+# store the features.txt into a variable
+features <-read.table("./data/features.txt")
+# select the desired features and save it into variable
+desired.features <- grep("std|mean", features[,2])
+mergeset <- mergeset[,desired.features]
